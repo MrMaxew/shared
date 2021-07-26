cca1dea6c2683e4c5666b99ca806a87390ef8da1
Max Leon <maxhleon@hotmail.com>
2021-07-26 14:07:54 -0300

    Initial Push

--- lib.py
+++
@@ -1,6 +0,0 @@
-def try_me():
-    print("Can you see this?")
-    answer = input()
-    print("I don't know what you said, but as you answered, it should work :)")
-    
-    
---
+++ shared/lib.py
@@ -0,0 +1,5 @@
+def try_me():
+    
+    print("Can you see this?")
+    res = input()
+    return print(f"I don't know what {res} means, but as you answered, it should work :)")
